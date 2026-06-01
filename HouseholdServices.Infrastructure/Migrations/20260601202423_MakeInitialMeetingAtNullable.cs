7b6dab3b3da659d541662de228dc009885dd9312
an3ks <avdienkodanya99@gmail.com>
2026-06-01 23:32:28 +0300

    Fixed migrations, updated swagger for authorized testing

---
+++ HouseholdServices.Infrastructure/Migrations/20260601202423_MakeInitialMeetingAtNullable.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HouseholdServices.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeInitialMeetingAtNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "initial_meeting_at",
+                table: "orders",
+                type: "timestamp with time zone",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "initial_meeting_at",
+                table: "orders",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, DateTimeKind.Utc),
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldNullable: true);
+        }
+    }
+}
