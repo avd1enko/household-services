a03a6ee101fef2a58da27bb004a273ee895f90b4
an3ks <avdienkodanya99@gmail.com>
2026-06-13 16:24:30 +0300

    Master Profile service + DTO done

---
+++ HouseholdServices.Application/DTOs/MasterProfile/UpdateMasterProfileRequest.cs
@@ -0,0 +1,7 @@
+namespace HouseholdServices.Application.DTOs.MasterProfile;
+
+public class UpdateMasterProfileRequest
+{
+    public string? Description { get; set; }
+    public int? ExperienceYears { get; set; }
+}
---
+++ HouseholdServices.Application/DTOs/ServiceCategories/ServiceCategoryResponse.cs
@@ -0,0 +1,7 @@
+namespace HouseholdServices.Application.DTOs.ServiceCategories;
+
+public class ServiceCategoryResponse
+{
+    public int CategoryId { get; set; }
+    public string Name { get; set; } = null!;
+}
