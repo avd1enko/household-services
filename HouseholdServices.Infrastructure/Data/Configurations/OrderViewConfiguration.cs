5de291b80b9a04310bb8c6a97d8f8d17ac888c23
an3ks <avdienkodanya99@gmail.com>
2026-06-14 20:59:24 +0300

    added mapper for orderView

--- HouseholdServices.Infrastructure/Data/Configurations/OrderViewConfiguration.cs
+++ HouseholdServices.Infrastructure/Data/Configurations/OrderViewConfiguration.cs
@@ -10,5 +10,30 @@ public class OrderViewConfiguration : IEntityTypeConfiguration<OrderView>
     {
         builder.HasNoKey();
         builder.ToView("order_details_view");
+
+        builder.Property(order => order.OrderId).HasColumnName("order_id");
+        builder.Property(order => order.Status).HasColumnName("status");
+        builder.Property(order => order.Price).HasColumnName("price");
+        builder.Property(order => order.InitialMeetingAt).HasColumnName("initial_meeting_at");
+        builder.Property(order => order.CreatedAt).HasColumnName("created_at");
+        builder.Property(order => order.CompletedAt).HasColumnName("completed_at");
+
+        builder.Property(order => order.RequestId).HasColumnName("request_id");
+        builder.Property(order => order.RequestTitle).HasColumnName("request_title");
+        builder.Property(order => order.RequestDescription).HasColumnName("request_description");
+        builder.Property(order => order.RequestAddress).HasColumnName("request_address");
+        builder.Property(order => order.DesiredDate).HasColumnName("desired_date");
+        builder.Property(order => order.CategoryId).HasColumnName("category_id");
+        builder.Property(order => order.CategoryName).HasColumnName("category_name");
+
+        builder.Property(order => order.ClientId).HasColumnName("client_id");
+        builder.Property(order => order.ClientFirstName).HasColumnName("client_first_name");
+        builder.Property(order => order.ClientLastName).HasColumnName("client_last_name");
+        builder.Property(order => order.ClientPhone).HasColumnName("client_phone");
+
+        builder.Property(order => order.MasterId).HasColumnName("master_id");
+        builder.Property(order => order.MasterFirstName).HasColumnName("master_first_name");
+        builder.Property(order => order.MasterLastName).HasColumnName("master_last_name");
+        builder.Property(order => order.MasterPhone).HasColumnName("master_phone");
     }
 }
